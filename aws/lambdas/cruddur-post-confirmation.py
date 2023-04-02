bb5b7a0ccaa6e9e49f1e5f06b0cdbf9ca7e6ea1d
ijamesink <James.Inkoom@icloud.com>
2023-04-02 03:07:28 +0000

    refactor db library and implement create activity

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -4,12 +4,29 @@ import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
+    print('UserAttributes')
+    print(user)
+
+
     user_display_name = user['name']
     user_handle = user['preferred_username']
     user_email = user ['email']
     user_cognito_id = user ['sub']
 
     try:
+        print('entered try')
+        sql = f"""
+        INSERT INTO users (
+            display_name, 
+            handle,
+            email, 
+            cognito_user_id
+        ) 
+        VALUES(%s, %s, %s, %s) 
+        """
+
+        print('SQL statement .....')
+        print(sql)
         conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         #     host=(os.getenv('PG_HOSTNAME')),
         #     database=(os.getenv('PG_DATABASE')),
@@ -17,23 +34,14 @@ def lambda_handler(event, context):
         #     password=(os.getenv('PG_SECRET'))
         # )
         cur = conn.cursor()
-      
-
-        sql = f"""
-            INSERT INTO users (
-                display_name, 
-                handle,
-                email, 
-                cognito_user_id
-            ) 
-            VALUES(
-                '{user_display_name}',
-                '{user_handle}', 
-                '{user_email}', 
-                '{user_cognito_id}'
-            ) 
-        """
-        cur.execute(sql)
+        params = [
+            user_display_name,
+            user_email,
+            user_handle,
+            user_cognito_id
+        ]
+    
+        cur.execute(sql, parameters)
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,7 +1,120 @@
 from psycopg_pool import ConnectionPool
 import os
+import re
+import sys
+from flask import current_app as app
 
-def query_wrap_object(template):
+
+class Db:
+  def __init__(self):
+    self.init_pool()
+
+  def template(self, *args):
+    pathing = list((app.root_path,'db','sql') + args)
+    pathing[-1] = pathing [-1] + '.sql'
+
+    template_path = os.path.join(*pathing)
+
+    green = '\033[92m'
+    no color = '\033[0m'
+    print('\n')
+    print(f'{green} load SQL template: {template_path} -----{no color}')
+
+    with open (template_path, 'r') as f:
+      template_content = f.read()
+    return template_content
+
+
+  def init_pool(self):
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+
+
+  def print_params(self, params):
+    blue = '\033[94m'
+    no color = '\033[0m'
+    print(f'{cyan}SQL Params: {no color}')
+    for key, value in params.items():
+      print(key, ':', value)
+
+
+  def print_sql(self, title, sql):
+    cyan = '\033[96m'
+    no color = '\033[0m'
+    print(f'{cyan}SQL Statement - [{title}] -----{no color}')
+    print(sql)
+
+  def query_commit_return_id(self, sql, params={}):
+    self.print_sql('commit with returning', sql)
+    
+    pattern = r'\bRETURNING\b'
+    is_returning_id = re.search(pattern, sql)
+    # if match:
+    #   print('found match!')
+    # else:
+    #   print('No match found'
+
+    try:
+        with self.pool.connection() as conn:
+          cur = onn.cursor()
+          cur.execute(sql, params)
+          if is_returning_id:
+            returning_id = cur.fetchone()[0]
+          conn.commit ()
+          if is_returning_id:
+            return returning_id
+
+      except Exception as err:
+        print_sql_err(err)
+        # conn.rollback()
+
+
+  # when we want to commit data such as insert
+  def query_commit(self,sql):
+    try:
+      conn = self.pool.connection()
+      cur = onn.cursor()
+      cur.execute(sql)
+      conn.commit ()
+
+    except Exception as err:
+      print_sql_err(err)
+      # conn.rollback()
+
+    
+  # when we want to return an array of json object
+  def query_array_json(self, sql, params):
+    self.print_sql('array', sql)
+
+    wrapped_sql  = self.query_wrap_array(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql, params)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        if json == None:
+          '{}'
+        else:
+          return json[0]
+
+
+  # when we want to return a json object
+  def query_object_json(self, sql, params):
+    self.print_sql('json', sql)
+    self.print_params(params)
+
+    wrapped_sql  = self.query_wrap_object(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql, params)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+
+
+  def query_wrap_object(template):
   sql = f"""
   (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
   {template}
@@ -10,14 +123,32 @@ def query_wrap_object(template):
   return sql
 
 
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+  def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
 
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+  def print_sql_err(err):
+  # get details about the exception
+  err_type, err_obj, traceback = sys.exc_info()
+
+  # get the line number when the exception occured
+  line_num = traceback.tb_lineno
+
+  # print the connect () error
+  print ('\npsycopg ERROR:', err, 'on line number:', line_num)
+  print ('psycopg traceback:', traceback, '---- type:', err_type)
+
+  # psycopg2 extensions.Diagnostics object attribute
+  # print ('\nextensions.Diagnostics:', err.diag)
+
+  # print the pgcode and pgerror exceptions
+  print ('pgerror:', err.pgerror)
+  print ('pgcode:', err.pgcode, '\n')
+
+
+db = Db()
